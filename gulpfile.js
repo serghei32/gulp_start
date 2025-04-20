d3865133276f7d37447e1261a14f422c5c2e0dfd
John Doe <johndoe@example.com>
2025-04-20 18:06:34 +0200

    add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-const scss =require('gulp-sass');  
+const { src, dest } = require('gulp');
+const sass = require('sass');
+const gulpSass = require('gulp-sass')(sass);
+const postcss = require('gulp-postcss');  // подключаем postcss
+const autoprefixer = require('autoprefixer');  // подключаем autoprefixer
+const cleanCSS = require('gulp-clean-css');
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(postcss([autoprefixer()]))  // используем autoprefixer через postcss
+        .pipe(cleanCSS())  // минифицируем CSS
+        .pipe(dest('app/css'));
+}
+
+exports.default = styles;
+
+
+
+
+
+
+  
+
